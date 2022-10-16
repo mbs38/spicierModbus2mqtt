6134ac861f44186e4560c50ffb0967a47a1457ce
Stefan Fischer <stefan-git@rx0.de>
2022-10-16 14:30:25 +0200

    ensure correct indent

--- modbus2mqtt/modbus2mqtt.py
+++ modbus2mqtt/modbus2mqtt.py
@@ -221,7 +221,7 @@ class Poller:
                             p.failcounter=3
                         p.failCount(p.failed)
                     if verbosity >= 1:
-                        print("MODBUS connection error, trying again...")
+                        print("MODBUS connection error (poller), trying again...")
 
     def checkPoll(self):
         if time.clock_gettime(0) >= self.next_due and not self.disabled:
@@ -283,14 +283,14 @@ class dataTypes:
             self.combine=self.combineString
             self.stringLength=length
             self.regAmount=int(length/2)
-        #elif conf == "int32LE":
-           # self.parse=self.parseint32LE
-           # self.combine=self.combineint32LE
-           # self.regAmount=2          
-        #elif conf == "int32BE":
-         #   self.regAmount=2
-          #  self.parse=self.parseint32BE
-           # self.combine=self.combineint32BE
+        elif conf == "int32LE":
+            self.parse=self.parseint32LE
+            self.combine=self.combineint32LE
+            self.regAmount=2
+        elif conf == "int32BE":
+            self.regAmount=2
+            self.parse=self.parseint32BE
+            self.combine=self.combineint32BE
         elif conf == "int16":
             self.regAmount=1         
             self.parse=self.parseint16
@@ -351,15 +351,6 @@ class dataTypes:
             out+=chr(x&0x00FF)
         return out
 
-    def parseint32LE(self,msg):
-        pass
-    def combineint32LE(self,val):
-        pass
-    
-    def parseint32BE(self,msg):
-        pass
-    def combineint32BE(self,val):
-        pass
     def parseint16(self,msg):
         try:
             value=int(msg)
@@ -409,6 +400,30 @@ class dataTypes:
         return out
     def combineuint32BE(self,val):
         out = val[0] + val[1]*65536
+        return out
+
+    def parseint32LE(self,msg):
+        try:
+            value=int(msg)
+            value = int.from_bytes(value.to_bytes(4, 'little', signed=False), 'little', signed=True)
+        except:
+            out=None
+        return out
+    def combineint32LE(self,val):
+        out = val[0]*65536 + val[1]
+        out = int.from_bytes(out.to_bytes(4, 'little', signed=False), 'little', signed=True)
+        return out
+
+    def parseint32BE(self,msg):
+        try:
+            value=int(msg)
+            value = int.from_bytes(value.to_bytes(4, 'big', signed=False), 'big', signed=True)
+        except:
+            out=None
+        return out
+    def combineint32BE(self,val):
+        out = val[0] + val[1]*65536
+        out = int.from_bytes(out.to_bytes(4, 'big', signed=False), 'big', signed=True)
         return out
     
     def parseuint16(self,msg):
@@ -812,7 +827,7 @@ def main():
                         p.failcounter=3
                     p.failCount(p.failed)
                 if verbosity >= 1:
-                    print("MODBUS connection error, trying again...")
+                    print("MODBUS connection error (mainLoop), trying again...")
     
         if not mqc.initial_connection_attempted:
            try:
@@ -828,7 +843,7 @@ def main():
                     print("MQTT Loop started")
            except:
                 if verbosity>=1:
-                  print("Socket Error connecting to MQTT broker: " + args.mqtt_host + ":" + str(mqtt_port) + ", check LAN/Internet connection, trying again...")
+                    print("Socket Error connecting to MQTT broker: " + args.mqtt_host + ":" + str(mqtt_port) + ", check LAN/Internet connection, trying again...")
     
         if mqc.initial_connection_made: #Don't start polling unless the initial connection to MQTT has been made, no offline MQTT storage will be available until then.
             if modbus_connected:
